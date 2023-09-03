30333e3a3fe9e4c56eef82d0ac541c147ab7fc49
MemologLeza <lizabakay1@gmail.com>
2023-09-03 18:55:50 +0100

    update phonebook app with Redux Toolkit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,37 +2,35 @@ import { nanoid } from 'nanoid';
 import Filter from './Filter/Filter';
 import ContactList from './ContactList/ContactList';
 import ContactForm from './ContactForm/ContactForm';
-import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { createContact, deleteContacts } from 'store/contacts/contactsSlice';
+import { setFilter } from 'store/filter/filterSlice';
 
 const App = () => {
-  const [contacts, setContacts] = useState([]);
-  const [filter, setFilter] = useState('');
-  useEffect(() => {
-    const localContacts = JSON.parse(localStorage.getItem('contacts'));
-    if (localContacts?.length > 0) setContacts(localContacts);
-  }, []);
+  const { contacts } = useSelector(state => state.contacts);
+  const { filter } = useSelector(state => state.filter);
+  const dispatch = useDispatch();
 
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
+  const handleDelete = id => {
+    dispatch(deleteContacts(id));
+  };
 
-  const createContact = (name, number) => {
+  const handleCreateContact = (name, number) => {
     if (isDuplicate(name)) return alert(`${name} is already in contacts(( `);
     const newContact = {
       id: nanoid(),
       name,
       number,
     };
-    setContacts(prev => [newContact, ...prev]);
+    dispatch(createContact(newContact));
   };
   const isDuplicate = name => contacts.find(contact => contact.name === name);
 
-  const handleDelete = id => {
-    setContacts(prev => prev.filter(contact => contact.id !== id));
-  };
   const onFilterChange = e => {
-    setFilter(e.currentTarget.value);
+    const value = e.currentTarget.value;
+    dispatch(setFilter(value));
   };
+
   const filterContacts = () => {
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
@@ -50,7 +48,7 @@ const App = () => {
     >
       <h1>Phonebook</h1>
       <div>
-        <ContactForm createContact={createContact} />
+        <ContactForm createContact={handleCreateContact} />
       </div>
       <div>
         <h2>Contacts</h2>
